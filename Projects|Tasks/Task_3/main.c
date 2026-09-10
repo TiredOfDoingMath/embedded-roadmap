f1ed0141a20b77eb67db5d422bca7ea8d45c52b9
TiredOfDoingMath <jared66078@gmail.com>
2026-09-10 18:11:18 -0400

    Update main.c

--- Projects|Tasks/Task_3/main.c
+++ Projects|Tasks/Task_3/main.c
@@ -1,1 +1,11 @@
+#include "stm32c0xx.h"
+int main(void) {
+  //masking pin 4 interrupts
+  EXTI -> IMR1 &= ~(1 << 4);
+  
+  //set pin 4 to trigger on rising edge
+  EXTI -> RTSR1 |= (1 << 4);
 
+
+  while(1){}
+}
