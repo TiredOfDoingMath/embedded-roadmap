98043233fea6e55f8cf8d11e634cee59a6fef482
TiredOfDoingMath <jared66078@gmail.com>
2026-09-18 20:22:12 -0400

    fixed several bugs in the previous functions and added some new functions for the EXTI driver

--- Projects|Tasks/Task_3/general.c
+++ Projects|Tasks/Task_3/general.c
@@ -1,14 +1,5 @@
-
-
-#define Port_A 0
-#define Port_B 1
-#define Port_C 2
-#define Port_D 3
-#define Port_F 5
-#define risingEdge true;
-#define fallingEdge false;
-
-
+#include "general.h"
+#include <stdint.h>
 //***function to reset all EXTICR modes***
 
 //each register (1,2,3,4) corresponds to (0,1,2,3) in the array
@@ -23,7 +14,7 @@ static void reset(uint8_t Register, uint8_t quad) {
 }
 
 // a much better implementation of setting ports
-void set(uint8_t Register, uint8_t quad, uint8_t port) {
+void set(uint8_t Register, uint8_t quad, PortType port) {
    uint8_t shift = 8 * (quad - 1);
    reset(Register, quad);
    switch (port) {
@@ -46,20 +37,14 @@ void set(uint8_t Register, uint8_t quad, uint8_t port) {
    }
 }
 
-
-typedef struct {
-  EXTI_TypeDef *ptr;
-  uint8_t line;
-} edgeConfig;
-
 //turning on and off and configuring rising edge and falling edge event/interrupt trigger
-void config(edgeConfig *self, bool edge, bool enable) {
+void config(edgeConfig *self, Edge_Type edge, bool enable) {
   if(edge == risingEdge && enable == true) {
     self -> ptr -> FTSR1 &= ~(1UL << self -> line);
     self -> ptr -> RTSR1 |= (1UL << self -> line);
   } else if(edge == fallingEdge && enable == true) {
     self -> ptr -> RTSR1 &= ~(1UL << self -> line);
-    self -> ptr -> FTSR1 |= ~(1UL << self -> line);
+    self -> ptr -> FTSR1 |= (1UL << self -> line);
   } else if(enable == false) {
     self -> ptr -> FTSR1 &= ~(1UL << self -> line);
     self -> ptr -> RTSR1 &= ~(1UL << self -> line);
@@ -70,3 +55,29 @@ void fireSWE(edgeConfig *self) {
   self -> ptr -> SWIER1 |= (1UL << self -> line);
 }
 
+//clearing rising edge pending event
+void clearRPR1(edgeConfig *self) {
+  self -> ptr -> RPR1 |= (1UL << self -> line);
+}
+
+//clearing falling edge pending event
+void clearFPR1(edgeConfig *self) {
+  self -> ptr -> FPR1 |= (1UL << self -> line);
+}
+
+
+void interrupt_Mask_Unmask(edgeConfig *self, Mask status) {
+  if(status == masked) {
+    self -> ptr -> IMR1 &= ~(1UL << self -> line);
+  } else if(status == unmasked) {
+    self -> ptr -> IMR1 |= (1UL << self -> line);
+  }
+}
+
+void event_Mask_Unmask(edgeConfig *self, Mask status) {
+  if(status == masked) {
+    self -> ptr -> EMR1 &= ~(1UL << self -> line);
+  } else if(status == unmasked) {
+    self -> ptr -> EMR1 |= (1UL << self -> line);
+  }
+}
